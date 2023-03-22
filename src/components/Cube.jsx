edc64e5512f277c1b4a5bb5ad8dd82483f9c358b
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2023-03-22 14:34:16 -0400

    atualização/website3D

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,8 +1,12 @@
 import { PerspectiveCamera, RenderTexture, Text } from "@react-three/drei";
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 
 const Cube = () => {
+const textRef = useRef()
+useFrame(state=> (textRef.current.position.x = Math.sin(state.clock.elapsedTime)) * 2)
+
   return (
     <mesh>
       <boxGeometry args={[2, 2, 2]} />
@@ -10,7 +14,7 @@ const Cube = () => {
         <RenderTexture attach="map">
           <PerspectiveCamera makeDefault position={[0, 0, 2]} />
           <color attach="background" args={["pink"]} />
-          <Text fontSize={1} color="#555">
+          <Text ref={textRef} fontSize={1} color="#555">
             Hello
           </Text>
         </RenderTexture>
